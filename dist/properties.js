dc52bc88be93e3ee1f83bb8ee12a60aebe3790f5
Steve <steve.neeson21@gmail.com>
2018-03-08 16:02:22 +0200

    updated plugin

--- dist/properties.js
+++ dist/properties.js
@@ -2,8 +2,9 @@ export const props = { browserPath: '"C:\\\\Program Files (x86)\\\\Google\\\\Chr
   localTomcatPath: 'C:\\\\DEV\\\\tomcat\\\\webapps',
   thisWebappName: 'IndirectWebAppLeons',
   phisicalServerName: 'https://mail.google.com/mail/u/0/',
-  versionPatch: '1',
-  versionBuildNumber: '1',
-  versionDate: '10072015',
+  versionPatch: 1,
+  versionBuildNumber: 1,
+  versionDate: 10072015,
   versionEnvString: 'LOCAL_TEST',
-  demoMode: 'false' }
+  demoMode: false,
+  demoModeIsTrue: true }
--- myPlugin/myPlugin.js
+++ myPlugin/myPlugin.js
@@ -12,8 +12,7 @@ class CompileProperties {
         compiler.plugin('compile', (params) => {
             fs.readFile(this.options.path, 'latin1', (err, data) => {
                 this.properties = createObject(data);
-
-                fs.writeFile(this.options.output, `export const props = ${util.inspect(this.properties)}`)
+                fs.writeFile(this.options.output, `export const props = ${util.inspect(this.properties)}`, () => console.log('done write'))
             });
 
         })
@@ -25,9 +24,27 @@ function createObject(data) {
     let match = regex.exec(data);
     const tempObj = {}
     while (match != null) {
-        tempObj[replaceDot(match[1])] = match[2];
+        tempObj[replaceDot(match[1])] = convertToBolAndNum(match[2]);
         match = regex.exec(data);
     }
+    console.log(tempObj)
+    return tempObj;
+}
+
+function convertToBolAndNum(string) {
+    if (string === 'true' || string === 'false') {
+        return string === 'true';
+    } else if (/^\d*$/.test(string)) {
+        return +string;
+    }
+    return string;
+}
+
+function transformToChar(obj) {
+    const tempObj = {};
+    Object.keys(obj).forEach(key => {
+        tempObj[decodeString(key)] = decodeString(obj[key]);
+    })
     return tempObj;
 }
 
